2fb98e3dae20ac3b5fb7f5b8064687a921ce0c02
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-02-02 02:35:51 +0100

    add

--- src/components/Map/styles.js
+++ src/components/Map/styles.js
@@ -5,7 +5,7 @@ export default makeStyles(() => ({
     padding: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'center', width: '100px',
   },
   mapContainer: {
-    height: '83vh', width: '90%',
+    height: '84vh', width: '90%',
   },
   markerContainer: {
     position: 'absolute', transform: 'translate(-50%, -50%)', zIndex: 1, '&:hover': { zIndex: 2 },
