7ba9eaad0caeb1f23d13b1e927d74912aaf8ed38
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2022-04-26 07:36:23 +0200

    add

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -7,7 +7,7 @@ export default makeStyles((theme) => ({
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block',
-      color: '#003045',
+      color: '#003046',
       
     
     },
